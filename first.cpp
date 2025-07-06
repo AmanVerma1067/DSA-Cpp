d1de6ef7d85554792b16e906c095aef1c8b2e4e7
Aman Verma <153915075+AmanVerma1067@users.noreply.github.com>
2025-07-06 20:09:17 +0530

    Update first.cpp

--- first.cpp
+++ first.cpp
@@ -10,10 +10,13 @@ int main(){
     cout<<"Enter a Number: ";
     cin>>n;
     cout<<n<<endl;
-    int arr[7] = {1,2,3,4,5,6,7};
-    for (int i = 0; i < 7; i++)
+//  int arr[7] = {1,2,3,4,5,6,7};
+      vector <int> arr = {1,2,3,4,5,6,7};
+// for (int i = 0; i < 7; i++)
+    for( auto it: arr)
     {
-        cout<<arr[i]*arr[i]<<endl;
+// cout<<arr[i]*arr[i]<<endl;
+          cout<<it*it<<endl;
     }
   
     return 0;
